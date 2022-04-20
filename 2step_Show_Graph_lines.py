cc2f326111d0b0027a246f8a010610a079e17bb4
Youngsoo Kim <61109490+Young0927@users.noreply.github.com>
2022-04-20 10:02:44 +0900

    Update 2step_Show_Graph_lines.py

--- 2step_Show_Graph_lines.py
+++ 2step_Show_Graph_lines.py
@@ -7,7 +7,13 @@ df = pd.DataFrame(df)
 
 import matplotlib.pyplot as plt
  
-## 3개 데이터 분리
+'''
+The throughput draws curve shape plots and becomes saturated when JIT1 is greater than 25 on certain WAC_time, Process_Time. 
+The height of S shape is decreasing when higher WAC_time, Process_Time is used. 
+However, in all four plots, throughput is saturated after JIT1 becomes 25. 
+This means that the cluster tool scheduler can effectively hide the transport time of robot arms when JIT1 is at least 25 seconds. 
+JIT1 value is the look-ahead time means determines when the wafer start to load from the port to the PM.
+'''
 a_df = df.query('Process_Time ==120 and WAC_Time ==90') 
 a_Throughput = a_df['Throughput']
 JIT1 = a_df['JIT1']
@@ -18,17 +24,17 @@ b_Throughput = b_df['Throughput']
 c_df = df.query('Process_Time ==180 and WAC_Time ==90')
 c_Throughput = c_df['Throughput']
  
-fig = plt.figure(figsize=(9,9)) ## 캔버스 생성
-fig.set_facecolor('white') ## 캔버스 색상 설정
-ax = fig.add_subplot() ## 그림 뼈대(프레임) 생성
+fig = plt.figure(figsize=(9,9)) ## create canvas
+fig.set_facecolor('white') ## canvas color
+ax = fig.add_subplot() ## create canvas frame
  
-ax.plot(JIT1,a_Throughput,marker='o',label='ProcessTime=120') ## 선그래프 생성
-ax.plot(JIT1,b_Throughput,marker='o',label='ProcessTime=150') 
-ax.plot(JIT1,c_Throughput,marker='o',label='ProcessTime=180') 
+ax.plot(JIT1,a_Throughput,marker='o',label='ProcessTime=120') ##draws line graph which has certain process time
+ax.plot(JIT1,b_Throughput,marker='o',label='ProcessTime=150') ##draws line graph which has certain process time
+ax.plot(JIT1,c_Throughput,marker='o',label='ProcessTime=180') ##draws line graph which has certain process time
  
-ax.legend() ## 범례
+ax.legend() ## setting of legend 
  
-plt.title('Throughput against JIT1 with different ProcessTime',fontsize=15) ## 타이틀 설정
+plt.title('Throughput against JIT1 with different ProcessTime',fontsize=15) ## setting of title
 plt.xlabel("JIT1",fontsize=10)
 plt.ylabel("Throughput",fontsize=10)
 plt.show()
